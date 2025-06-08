a0a096b62c560139e8877e94ba728b34459e6df2
Kiyonori SAKAI <neco.am@gmail.com>
2025-06-08 12:00:14 +0900

    💯 # 11 test_must_not_検索条件を含めて_toArray_メソッドを呼んだ場合、意図したクエリの形が組み立てられること

--- tests/Builders/FluentQueryBuilderTest.php
+++ tests/Builders/FluentQueryBuilderTest.php
@@ -161,4 +161,66 @@ class FluentQueryBuilderTest extends TestCase
             actual: $result,
         );
     }
+
+    public function test_must_not_検索条件を含めて_toArray_メソッドを呼んだ場合、意図したクエリの形が組み立てられること()
+    {
+        $searchQuery = new FluentQueryBuilder;
+
+        $result = $searchQuery
+            ->mustNot(function (Builder $mustNot) {
+                $mustNot
+                    ->term('chat_id', 'u968edd043c46262efe69ef21ad458c6d')
+                    ->term('type', 1)
+                    ->match('content', 'おはよう')
+                    ->match('content', '今度は')
+                    ->range(
+                        'created_time',
+                        1777777777777,
+                        1888888888888,
+                    );
+            })
+            ->toArray();
+
+        $this->assertSame(
+            expected: [
+                'body' => [
+                    'query' => [
+                        'bool' => [
+                            'must_not' => [
+                                [
+                                    'term' => [
+                                        'chat_id' => 'u968edd043c46262efe69ef21ad458c6d',
+                                    ],
+                                ],
+                                [
+                                    'term' => [
+                                        'type' => 1,
+                                    ],
+                                ],
+                                [
+                                    'match' => [
+                                        'content' => 'おはよう',
+                                    ],
+                                ],
+                                [
+                                    'match' => [
+                                        'content' => '今度は',
+                                    ],
+                                ],
+                                [
+                                    'range' => [
+                                        'created_time' => [
+                                            'gte' => 1777777777777,
+                                            'lte' => 1888888888888,
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            actual: $result,
+        );
+    }
 }

2dc07b74473d2dda47b7b635836a1863c18fb371
Kiyonori SAKAI <neco.am@gmail.com>
2025-06-11 05:48:25 +0900

    💯 # 18 test_highlight_検索条件を含めて_toArray_メソッドを呼んだ場合、意図したクエリの形が組み立てられること

--- tests/Builders/FluentQueryBuilderTest.php
+++ tests/Builders/FluentQueryBuilderTest.php
@@ -4,7 +4,9 @@
 namespace Tests\Builders;
 
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\FluentQueryBuilder;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\Highlight;
 use Kiyonori\ElasticsearchFluentQueryBuilder\Values\Nothing;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Values\StaticStd;
 use PHPUnit\Framework\TestCase;
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\Builder;
 
@@ -216,6 +218,165 @@ class FluentQueryBuilderTest extends TestCase
                                     ],
                                 ],
                             ],
+                        ],
+                    ],
+                ],
+            ],
+            actual: $result,
+        );
+    }
+
+    public function test_highlight_検索条件を含めて_toArray_メソッドを呼んだ場合、意図したクエリの形が組み立てられること()
+    {
+        $searchQuery = new FluentQueryBuilder;
+
+        $result = $searchQuery
+            ->highlight(function (Highlight $highlightField) {
+                $highlightField->fields('content');
+            })
+            ->toArray();
+
+        $this->assertSame(
+            expected: [
+                'body' => [
+                    'highlight' => [
+                        'fields' => [
+                            'content' => StaticStd::instance(),
+                        ],
+                    ],
+                ],
+            ],
+            actual: $result,
+        );
+
+        $result = $searchQuery
+            ->highlight(
+                fn(Highlight $highlightField) => $highlightField
+                    ->fields(
+                        fieldName: 'content',
+                        fragmentSize: 150,
+                        numberOfFragments: 3,
+                    ),
+            )
+            ->toArray();
+
+        $this->assertSame(
+            expected: [
+                'body' => [
+                    'highlight' => [
+                        'fields' => [
+                            'content' => [
+                                'fragment_size'       => 150,
+                                'number_of_fragments' => 3,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            actual: $result,
+        );
+
+        $result = $searchQuery
+            ->highlight(
+                fn(Highlight $highlightField) => $highlightField
+                    ->fields(
+                        fieldName: 'content',
+                        fragmentSize: 150,
+                        numberOfFragments: 3,
+                    ),
+                preTags: ['<em>'],
+                postTags: ['</em>'],
+            )
+            ->toArray();
+
+        $this->assertSame(
+            expected: [
+                'body' => [
+                    'highlight' => [
+                        'pre_tags'  => ['<em>'],
+                        'post_tags' => ['</em>'],
+                        'fields'    => [
+                            'content' => [
+                                'fragment_size'       => 150,
+                                'number_of_fragments' => 3,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            actual: $result,
+        );
+
+        $result = $searchQuery
+            ->filter(function (Builder $filter) {
+                $filter
+                    ->term('chat_id', 'u968edd043c46262efe69ef21ad458c6d')
+                    ->term('type', 1)
+                    ->match('content', 'おはよう')
+                    ->match('content', '今度は')
+                    ->range(
+                        'created_time',
+                        1777777777777,
+                        1888888888888,
+                    );
+            })
+            ->highlight(
+                fn(Highlight $highlightField) => $highlightField
+                    ->fields(
+                        fieldName: 'content',
+                        fragmentSize: 150,
+                        numberOfFragments: 3,
+                    )
+                    ->fields(
+                        fieldName: 'chat_id',
+                    ),
+            )
+            ->toArray();
+
+        $this->assertSame(
+            expected: [
+                'body' => [
+                    'query'     => [
+                        'bool' => [
+                            'filter' => [
+                                [
+                                    'term' => [
+                                        'chat_id' => 'u968edd043c46262efe69ef21ad458c6d',
+                                    ],
+                                ],
+                                [
+                                    'term' => [
+                                        'type' => 1,
+                                    ],
+                                ],
+                                [
+                                    'match' => [
+                                        'content' => 'おはよう',
+                                    ],
+                                ],
+                                [
+                                    'match' => [
+                                        'content' => '今度は',
+                                    ],
+                                ],
+                                [
+                                    'range' => [
+                                        'created_time' => [
+                                            'gte' => 1777777777777,
+                                            'lte' => 1888888888888,
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'highlight' => [
+                        'fields' => [
+                            'content' => [
+                                'fragment_size'       => 150,
+                                'number_of_fragments' => 3,
+                            ],
+                            'chat_id' => StaticStd::instance(),
                         ],
                     ],
                 ],

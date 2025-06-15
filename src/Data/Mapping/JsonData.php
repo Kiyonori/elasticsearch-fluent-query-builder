76ca6b7ed0a97723946c51c1455c254ef216a295
Kiyonori SAKAI <neco.am@gmail.com>
2025-06-16 07:22:01 +0900

    🧙 # 21 ネストされた Data Transfer Object を再帰的に array にキャストする

--- src/Data/Mapping/JsonData.php
+++ src/Data/Mapping/JsonData.php
@@ -16,4 +16,33 @@ final readonly class JsonData
             body: $body
         );
     }
+
+    public function toArray(): array
+    {
+        return $this->_toArray($this);
+    }
+
+    private function _toArray(mixed $objectInstance): array
+    {
+        $output = [];
+
+        $properties = get_object_vars($objectInstance);
+
+        foreach ($properties as $key => $property) {
+            $isPrimitive = in_array(
+                needle: gettype($property),
+                haystack: ['boolean', 'integer', 'double', 'string', 'array', 'NULL'],
+                strict: true,
+            );
+
+            if ($isPrimitive) {
+                $output[$key] = $property;
+                continue;
+            }
+
+            $output[$key] = $this->_toArray($property);
+        }
+
+        return $output;
+    }
 }

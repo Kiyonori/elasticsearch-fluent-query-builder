4422be50690ee9cff42919ddd969ef4d71d6ae26
Kiyonori SAKAI <neco.am@gmail.com>
2025-06-18 07:08:58 +0900

    🧙 # 23 Explicit Mapping 用の JSON ファイルを読み込み、そのファイル内容を Data Transfer Object として返す

---
+++ src/ReadMappingJson.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kiyonori\ElasticsearchFluentQueryBuilder;
+
+use Kiyonori\ElasticsearchFluentQueryBuilder\Data\Mapping\JsonData;
+
+final readonly class ReadMappingJson
+{
+    /**
+     * Explicit Mapping 用の JSON ファイルを読み込み、そのファイル内容を Data Transfer Object として返す
+     */
+    public function execute(
+        string $jsonFilePath
+    ): JsonData {
+        /** @var string $contents */
+        $contents = file_get_contents($jsonFilePath);
+
+        $mappings = json_decode(
+            json: $contents,
+            associative: true,
+        );
+
+        $jsonData = new JsonData(
+            $mappings['index'],
+            $mappings['body'],
+        );
+
+        return $jsonData;
+    }
+}

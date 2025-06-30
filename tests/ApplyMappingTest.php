1496876a8bd674e5bd5053e3675c610c8ce3f676
Kiyonori SAKAI <neco.am@gmail.com>
2025-07-01 07:01:23 +0900

    💯 # 56 ApplyMapping のテストをもう少し充実させた

--- tests/ApplyMappingTest.php
+++ tests/ApplyMappingTest.php
@@ -3,20 +3,49 @@
 namespace Tests;
 
 use Kiyonori\ElasticsearchFluentQueryBuilder\ApplyMapping;
+use Kiyonori\ElasticsearchFluentQueryBuilder\CheckIndexExists;
+use Kiyonori\ElasticsearchFluentQueryBuilder\DeleteIndex;
 use PHPUnit\Framework\TestCase;
 
 class ApplyMappingTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        app(DeleteIndex::class)->execute(
+            indexName: 'chat_histories',
+            suppressNotFoundException: true,
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        app(DeleteIndex::class)->execute(
+            indexName: 'chat_histories',
+            suppressNotFoundException: true,
+        );
+    }
+
     public function test_JSON_ファイルからElasticsearchに事前にマッピングできること()
     {
-        $jsonFilePath = realpath(__DIR__ . '/storages/explicit_mapping/chat_histories.json');
+        $this->assertFalse(
+            app(CheckIndexExists::class)->execute(
+                indexName: 'chat_histories'
+            )
+        );
 
         $newIndexName = app(ApplyMapping::class)->execute(
-            $jsonFilePath
+            jsonFilePath: realpath(__DIR__ . '/storages/explicit_mapping/chat_histories.json'),
+        );
+
+        $this->assertStringStartsWith(
+            prefix: 'chat_histories_',
+            string: $newIndexName,
         );
 
         $this->assertTrue(
-            gettype($newIndexName) === 'string'
+            app(CheckIndexExists::class)->execute(
+                indexName: 'chat_histories'
+            )
         );
     }
 }

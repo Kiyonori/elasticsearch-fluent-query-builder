f5bb6f52e5e73db72d0337df414d034acdb55534
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-06 05:43:12 +0900

    🧙 # 69 MustQuery

--- src/Builders/MustQuery.php
+++ src/Builders/MustQuery.php
@@ -2,9 +2,58 @@
 
 namespace Kiyonori\ElasticsearchFluentQueryBuilder\Builders;
 
+use Kiyonori\ElasticsearchFluentQueryBuilder\Values\Nothing;
+
 final class MustQuery
 {
     private array $terms = [];
 
     private array $matches = [];
+
+    public function term(
+        string $fieldName,
+        mixed $value,
+    ): self {
+        $this->terms[] = [
+            'term' => [
+                $fieldName => $value,
+            ],
+        ];
+
+        return $this;
+    }
+
+    public function match(
+        string $fieldName,
+        mixed $value,
+    ): self {
+        $this->matches[] = [
+            'match' => [
+                $fieldName => $value,
+            ],
+        ];
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        $result = [
+            'must' => (function () {
+                if (count($this->terms) >= 1) {
+                    return $this->terms;
+                }
+
+                if (count($this->matches) >= 1) {
+                    return $this->matches;
+                }
+
+                return Nothing::make();
+            })(),
+        ];
+
+        return app(UnsetNothingKeyInArray::class)->execute(
+            $result
+        );
+    }
 }

45337fa9299880d7f128fe4295846815d467d0e9
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-06 05:59:16 +0900

    💯 # 69 houldQuery−＞term（）は、意図したクエリの形を組み立てること

---
+++ tests/Builders/ShouldQueryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\ShouldQuery;
+
+test(
+    'ShouldQuery−＞term（）は、意図したクエリの形を組み立てること',
+    function () {
+        /** @var ShouldQuery $should */
+        $should = app(ShouldQuery::class);
+
+        $result = $should
+            ->term('field_1', 'value 111')
+            ->term('field_2', 222.2)
+            ->minimumShouldMatch(1)
+            ->toArray();
+
+        expect($result)->toBe([
+            'should' => [
+                ['term' => ['field_1' => 'value 111']],
+                ['term' => ['field_2' => 222.2]],
+            ],
+            'minimum_should_match' => 1,
+        ]);
+    }
+);

36b28f1953dfbb91f2dcb5780b90767c5be8751d
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-09 06:07:09 +0900

    💯 # 69 ShouldQuery のコンストラクタに QueryTypeEnum::BOOL を指定すると bool というキー名にラッピングされた値が組み立てられること

--- tests/Builders/ShouldQueryTest.php
+++ tests/Builders/ShouldQueryTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\ShouldQuery;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Enums\QueryTypeEnum;
 
 test(
     'ShouldQuery−＞term（）は、意図したクエリの形を組み立てること',
@@ -66,6 +67,33 @@ test(
                 ['term' => ['field_3' => true]],
             ],
             // 'minimum_should_match' => 2,
+        ]);
+    }
+);
+
+test(
+    'ShouldQuery のコンストラクタに QueryTypeEnum::BOOL を指定すると bool というキー名にラッピングされた値が組み立てられること',
+    function () {
+        /** @var ShouldQuery $should */
+        $should = app(
+            ShouldQuery::class,
+            QueryTypeEnum::BOOL,
+        );
+
+        $result = $should
+            ->term('field_1', 'value 111')
+            ->term('field_2', 222.2)
+            ->minimumShouldMatch(1)
+            ->toArray();
+
+        expect($result)->toBe([
+            'bool' => [
+                'should' => [
+                    ['term' => ['field_1' => 'value 111']],
+                    ['term' => ['field_2' => 222.2]],
+                ],
+                'minimum_should_match' => 1,
+            ],
         ]);
     }
 );

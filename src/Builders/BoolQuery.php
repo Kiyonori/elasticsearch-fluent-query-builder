4dfd4719bdec84806a48adb83b26f9dcb38f832b
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-11 14:24:44 +0900

    ℹ️🧙💯️ WIP BoolQuery

--- src/Builders/BoolQuery.php
+++ src/Builders/BoolQuery.php
@@ -2,76 +2,22 @@
 
 namespace Kiyonori\ElasticsearchFluentQueryBuilder\Builders;
 
-use Kiyonori\ElasticsearchFluentQueryBuilder\Values\Nothing;
+use Closure;
 
 final class BoolQuery
 {
-    private array $terms = [];
+    private array $bools = [];
 
-    private array $matches = [];
-
-    private ?int $minimumMatch = null;
-
-    public static function should(): self
-    {
-        return new self;
-    }
-
-    public function term(
-        string $fieldName,
-        mixed $value,
+    public function must(
+        Closure $callback,
     ): self {
-        $this->terms[] = [
-            'term' => [
-                $fieldName => $value,
-            ],
-        ];
+        /** @var Fluent $fluent */
+        $fluent = app(Fluent::class);
+
+        $callback($fluent);
+
+        $this->bools[]['bool'] = $fluent->toArray();
 
         return $this;
-    }
-
-    public function match(
-        string $fieldName,
-        mixed $value,
-    ): self {
-        $this->matches[] = [
-            'match' => [
-                $fieldName => $value,
-            ],
-        ];
-
-        return $this;
-    }
-
-    public function minimumShouldMatch(
-        int $minimumMatch,
-    ): self {
-        $this->minimumMatch = $minimumMatch;
-
-        return $this;
-    }
-
-    public function toArray(): array
-    {
-        $result = [
-            'bool' => [
-                'should' => (function () {
-                    if (count($this->terms) >= 1) {
-                        return $this->terms;
-                    }
-
-                    if (count($this->matches) >= 1) {
-                        return $this->matches;
-                    }
-
-                    return Nothing::make();
-                })(),
-                'minimum_should_match' => $this->minimumMatch ?? Nothing::make(),
-            ],
-        ];
-
-        return app(UnsetNothingKeyInArray::class)->execute(
-            $result
-        );
     }
 }

42ec6d14d90faee863d09b7612af6cbb441f078c
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-14 12:53:41 +0900

    💯 # 69 MustQuery の bool メソッドが正しく機能すること

--- tests/Builders/MustQueryTest.php
+++ tests/Builders/MustQueryTest.php
@@ -1,60 +1,71 @@
 <?php
 
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\MustQuery;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\ShouldQuery;
+
 test(
     'MustQuery の bool メソッドが正しく機能すること',
     function () {
-        // TODO 無効なテストなので直すこと
+        /** @var MustQuery $must
+         */
+        $must = app(MustQuery::class);
 
-        // /** @var MustQuery $must */
-        // $must = app(MustQuery::class);
-        //
-        // $result = $must
-        //     ->bool(
-        //         function (Fluent $must) {
-        //             $must
-        //                 ->bool(
-        //                     function (ShouldQuery $should) {
-        //                         $should
-        //                             ->match('field_1', 'value1')
-        //                             ->match('field_2', 'value 2')
-        //                             ->minimumShouldMatch(1);
-        //                     }
-        //                 )
-        //                 ->bool(
-        //                     function (ShouldQuery $should) {
-        //                         $should
-        //                             ->match('field_3', 'value 333')
-        //                             ->match('field_4', 'value四')
-        //                             ->match('field_5', 'value５')
-        //                             ->minimumShouldMatch(2);
-        //                     }
-        //                 );
-        //         }
-        //     )
-        //     ->toArray();
-        //
-        // expect($result)->toBe([
-        //     'bool' => [
-        //         [
-        //             'bool' => [
-        //                 'should' => [
-        //                     ['match' => ['field_1' => 'value1']],
-        //                     ['match' => ['field_2' => 'value 2']],
-        //                 ],
-        //                 'minimum_should_match' => 1,
-        //             ],
-        //         ],
-        //         [
-        //             'bool' => [
-        //                 'should' => [
-        //                     ['match' => ['field_3' => 'value 333']],
-        //                     ['match' => ['field_4' => 'value四']],
-        //                     ['match' => ['field_5' => 'value５']],
-        //                 ],
-        //                 'minimum_should_match' => 2,
-        //             ],
-        //         ],
-        //     ],
-        // ]);
+        $result = $must
+            ->bool(
+                function (ShouldQuery $should) {
+                    $should
+                        ->match('field_1', 'value1')
+                        ->match('field_2', 'value 2');
+                },
+                minimumShouldMatch: 1,
+            )
+            ->bool(
+                function (ShouldQuery $should) {
+                    $should
+                        ->match('field_3', 'value 333')
+                        ->match('field_4', 'value四')
+                        ->match('field_5', 'value５');
+                },
+                minimumShouldMatch: 2,
+            )
+            ->toArray();
+
+        expect($result)->toBe([
+            [
+                'should' => [
+                    [
+                        'match' => [
+                            'field_1' => 'value1',
+                        ],
+                    ],
+                    [
+                        'match' => [
+                            'field_2' => 'value 2',
+                        ],
+                    ],
+                ],
+                'minimum_should_match' => 1,
+            ],
+            [
+                'should' => [
+                    [
+                        'match' => [
+                            'field_3' => 'value 333',
+                        ],
+                    ],
+                    [
+                        'match' => [
+                            'field_4' => 'value四',
+                        ],
+                    ],
+                    [
+                        'match' => [
+                            'field_5' => 'value５',
+                        ],
+                    ],
+                ],
+                'minimum_should_match' => 2,
+            ],
+        ]);
     }
-)->skip();
+);

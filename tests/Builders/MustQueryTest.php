09ae346c98f2b7ce53edfedbf74321ff1288ca51
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-15 20:04:27 +0900

    💯 # 69 MustQuery の term，match，range メソッドが正しく機能すること

--- tests/Builders/MustQueryTest.php
+++ tests/Builders/MustQueryTest.php
@@ -69,3 +69,55 @@ test(
         ]);
     }
 );
+
+test(
+    'MustQuery の term，match，range メソッドが正しく機能すること',
+    function () {
+        /** @var MustQuery $must */
+        $must = app(MustQuery::class);
+
+        $result = $must
+            ->term('chat_id', 'u968edd043c46262efe69ef21ad458c6d')
+            ->term('type', 1)
+            ->match('content', 'おはよう')
+            ->match('content', '今度')
+            ->range(
+                fieldName: 'created_time',
+                gte: 1736925752798,
+            )
+            ->range('id', 333, 555)
+            ->toArray();
+
+        expect($result)->toBe([
+            'must' => [
+                [
+                    'term' => ['chat_id' => 'u968edd043c46262efe69ef21ad458c6d'],
+                ],
+                [
+                    'term' => ['type' => 1],
+                ],
+                [
+                    'match' => ['content' => 'おはよう'],
+                ],
+                [
+                    'match' => ['content' => '今度'],
+                ],
+                [
+                    'range' => [
+                        'created_time' => [
+                            'gte' => 1736925752798,
+                        ],
+                    ],
+                ],
+                [
+                    'range' => [
+                        'id' => [
+                            'gte' => 333,
+                            'lte' => 555,
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+);

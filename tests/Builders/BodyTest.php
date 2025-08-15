9b037c215e939925cc6a7ff20cee6d9fd1a7489d
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-15 12:35:06 +0900

    💯 # 69 Body インスタンスの toArray メソッドは意図した形の array を出力すること その3

--- tests/Builders/BodyTest.php
+++ tests/Builders/BodyTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\Body;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\MustQuery;
 
 test(
     'Body インスタンスの toArray メソッドは意図した形の array を出力すること その1',
@@ -37,6 +38,62 @@ test(
                 'search_after' => [
                     123,
                     333,
+                ],
+            ],
+        ]);
+    }
+);
+
+test(
+    'Body インスタンスの toArray メソッドは意図した形の array を出力すること その3',
+    function () {
+        $result = Body::query(
+            function (MustQuery $must) {
+                $must
+                    ->term('chat_id', 'u968edd043c46262efe69ef21ad458c6d')
+                    ->term('type', 1)
+                    ->match('content', 'おはよう')
+                    ->match('content', '今度')
+                    ->range(
+                        fieldName: 'created_time',
+                        gte: 1736925752798,
+                    )
+                    ->range('id', 333, 555);
+            }
+        )->toArray();
+
+        expect($result)->toBe([
+            'body' => [
+                'query' => [
+                    'must' => [
+                        [
+                            'term' => ['chat_id' => 'u968edd043c46262efe69ef21ad458c6d'],
+                        ],
+                        [
+                            'term' => ['type' => 1],
+                        ],
+                        [
+                            'match' => ['content' => 'おはよう'],
+                        ],
+                        [
+                            'match' => ['content' => '今度'],
+                        ],
+                        [
+                            'range' => [
+                                'created_time' => [
+                                    'gte' => 1736925752798,
+                                ],
+                            ],
+                        ],
+                        [
+                            'range' => [
+                                'id' => [
+                                    'gte' => 333,
+                                    'lte' => 555,
+                                ],
+                            ],
+                        ],
+                    ],
                 ],
             ],
         ]);

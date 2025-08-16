19d320c137574bb13fdb408f19c66f22e8128016
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-17 05:43:21 +0900

    💯 # 69 FilterQuery の bool メソッドが正しく機能すること

---
+++ tests/Builders/FilterQueryTest.php
@@ -0,0 +1,76 @@
+<?php
+
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\FilterQuery;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\ShouldQuery;
+
+test(
+    'FilterQuery の bool メソッドが正しく機能すること',
+    function () {
+        /** @var FilterQuery $filter */
+        $filter = app(FilterQuery::class);
+
+        $result = $filter
+            ->bool(
+                function (ShouldQuery $should) {
+                    $should
+                        ->match('field_1', 'value1')
+                        ->match('field_2', 'value 2');
+                },
+                minimumShouldMatch: 1,
+            )
+            ->bool(
+                function (ShouldQuery $should) {
+                    $should
+                        ->match('field_3', 'value 333')
+                        ->match('field_4', 'value四')
+                        ->match('field_5', 'value５');
+                },
+                minimumShouldMatch: 2,
+            )
+            ->toArray();
+
+        expect($result)->toBe([
+            'filter' => [
+                [
+                    'bool' => [
+                        'should' => [
+                            [
+                                'match' => [
+                                    'field_1' => 'value1',
+                                ],
+                            ],
+                            [
+                                'match' => [
+                                    'field_2' => 'value 2',
+                                ],
+                            ],
+                        ],
+                        'minimum_should_match' => 1,
+                    ],
+                ],
+                [
+                    'bool' => [
+                        'should' => [
+                            [
+                                'match' => [
+                                    'field_3' => 'value 333',
+                                ],
+                            ],
+                            [
+                                'match' => [
+                                    'field_4' => 'value四',
+                                ],
+                            ],
+                            [
+                                'match' => [
+                                    'field_5' => 'value５',
+                                ],
+                            ],
+                        ],
+                        'minimum_should_match' => 2,
+                    ],
+                ],
+            ],
+        ]);
+    }
+);

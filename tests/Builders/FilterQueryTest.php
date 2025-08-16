fc386b4e7144a4d78f1b2b934f830739e6b64186
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-17 05:48:20 +0900

    💯 # 69 FilterQuery-＞term（）-＞match（）を併用した場合、意図したクエリの形を組み立てること

--- tests/Builders/FilterQueryTest.php
+++ tests/Builders/FilterQueryTest.php
@@ -116,3 +116,46 @@ test(
         ]);
     }
 );
+
+test(
+    'FilterQuery-＞term（）-＞match（）を併用した場合、意図したクエリの形を組み立てること',
+    function () {
+        /** @var FilterQuery $filter */
+        $filter = app(FilterQuery::class);
+
+        $result = $filter
+            ->term('field_1', 'value 1')
+            ->term('field_2', 222)
+            ->match('field_3', 'おはよう')
+            ->match('field_4', 'param444')
+            ->range(
+                fieldName: 'created_at',
+                gte: 1777777777777,
+            )
+            ->toArray();
+
+        expect($result)->toBe([
+            'filter' => [
+                [
+                    'term' => ['field_1' => 'value 1'],
+                ],
+                [
+                    'term' => ['field_2' => 222],
+                ],
+                [
+                    'match' => ['field_3' => 'おはよう'],
+                ],
+                [
+                    'match' => ['field_4' => 'param444'],
+                ],
+                [
+                    'range' => [
+                        'created_at' => [
+                            'gte' => 1777777777777,
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+);

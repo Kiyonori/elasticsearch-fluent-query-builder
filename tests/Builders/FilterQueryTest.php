d2ee86e43b8ac097867a03219e8030bacaf21312
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-17 05:44:07 +0900

    💯 # 69 FilterQuery−＞term（）は、意図したクエリの形を組み立てること

--- tests/Builders/FilterQueryTest.php
+++ tests/Builders/FilterQueryTest.php
@@ -74,3 +74,23 @@ test(
         ]);
     }
 );
+
+test(
+    'FilterQuery−＞term（）は、意図したクエリの形を組み立てること',
+    function () {
+        /** @var FilterQuery $filter */
+        $filter = app(FilterQuery::class);
+
+        $result = $filter
+            ->term('field_1', 'value 111')
+            ->term('field_2', 222.2)
+            ->toArray();
+
+        expect($result)->toBe([
+            'filter' => [
+                ['term' => ['field_1' => 'value 111']],
+                ['term' => ['field_2' => 222.2]],
+            ],
+        ]);
+    }
+);

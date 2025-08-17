c7c2854412e789c4d0f63db28acc9431876fb193
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-17 16:23:38 +0900

    💯 # 69 body.query 直下に must は配置できないので、テストを訂正

--- tests/Builders/BodyTest.php
+++ tests/Builders/BodyTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\Body;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\BoolQuery;
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\MustQuery;
 
 test(
@@ -48,48 +49,54 @@ test(
     'Body インスタンスの toArray メソッドは意図した形の array を出力すること その3',
     function () {
         $result = Body::query(
-            function (MustQuery $must) {
-                $must
-                    ->term('chat_id', 'u968edd043c46262efe69ef21ad458c6d')
-                    ->term('type', 1)
-                    ->match('content', 'おはよう')
-                    ->match('content', '今度')
-                    ->range(
-                        fieldName: 'created_time',
-                        gte: 1736925752798,
-                    )
-                    ->range('id', 333, 555);
+            function (BoolQuery $bool) {
+                $bool(
+                    function (MustQuery $must) {
+                        $must
+                            ->term('chat_id', 'u968edd043c46262efe69ef21ad458c6d')
+                            ->term('type', 1)
+                            ->match('content', 'おはよう')
+                            ->match('content', '今度')
+                            ->range(
+                                fieldName: 'created_time',
+                                gte: 1736925752798,
+                            )
+                            ->range('id', 333, 555);
+                    }
+                );
             }
         )->toArray();
 
         expect($result)->toBe([
             'body' => [
                 'query' => [
-                    'must' => [
-                        [
-                            'term' => ['chat_id' => 'u968edd043c46262efe69ef21ad458c6d'],
-                        ],
-                        [
-                            'term' => ['type' => 1],
-                        ],
-                        [
-                            'match' => ['content' => 'おはよう'],
-                        ],
-                        [
-                            'match' => ['content' => '今度'],
-                        ],
-                        [
-                            'range' => [
-                                'created_time' => [
-                                    'gte' => 1736925752798,
+                    'bool' => [
+                        'must' => [
+                            [
+                                'term' => ['chat_id' => 'u968edd043c46262efe69ef21ad458c6d'],
+                            ],
+                            [
+                                'term' => ['type' => 1],
+                            ],
+                            [
+                                'match' => ['content' => 'おはよう'],
+                            ],
+                            [
+                                'match' => ['content' => '今度'],
+                            ],
+                            [
+                                'range' => [
+                                    'created_time' => [
+                                        'gte' => 1736925752798,
+                                    ],
                                 ],
                             ],
-                        ],
-                        [
-                            'range' => [
-                                'id' => [
-                                    'gte' => 333,
-                                    'lte' => 555,
+                            [
+                                'range' => [
+                                    'id' => [
+                                        'gte' => 333,
+                                        'lte' => 555,
+                                    ],
                                 ],
                             ],
                         ],

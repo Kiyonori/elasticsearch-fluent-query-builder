3fd93d818662c26f3594a84b429c97ed56980c78
Kiyonori SAKAI <neco.am@gmail.com>
2025-08-19 05:55:07 +0900

    💯 #69 BoolQuery の toArray メソッドは意図した形の array を出力すること その2

--- tests/Builders/BoolQueryTest.php
+++ tests/Builders/BoolQueryTest.php
@@ -2,6 +2,7 @@
 
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\BoolQuery;
 use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\MustQuery;
+use Kiyonori\ElasticsearchFluentQueryBuilder\Builders\ShouldQuery;
 
 test(
     'BoolQuery の toArray メソッドは意図した形の array を出力すること その1',
@@ -55,6 +56,65 @@ test(
                         ],
                     ],
                 ],
+            ],
+        ]);
+    }
+);
+
+test(
+    'BoolQuery の toArray メソッドは意図した形の array を出力すること その2',
+    function () {
+        /** @var BoolQuery $bool */
+        $bool = app(BoolQuery::class);
+
+        $result = $bool(
+            function (ShouldQuery $should) {
+                $should
+                    ->term('chat_id', 'u968edd043c46262efe69ef21ad458c6d')
+                    ->term('type', 1)
+                    ->match('content', 'おはよう')
+                    ->range(
+                        fieldName: 'created_time',
+                        gte: 1736925752798,
+                    )
+                    ->range(
+                        fieldName: 'id',
+                        gte: 333,
+                        lte: 555,
+                    );
+            },
+            minimumShouldMatch: 1,
+        )->toArray();
+
+        expect($result)->toBe([
+            'bool' => [
+                'should' => [
+                    [
+                        'term' => ['chat_id' => 'u968edd043c46262efe69ef21ad458c6d'],
+                    ],
+                    [
+                        'term' => ['type' => 1],
+                    ],
+                    [
+                        'match' => ['content' => 'おはよう'],
+                    ],
+                    [
+                        'range' => [
+                            'created_time' => [
+                                'gte' => 1736925752798,
+                            ],
+                        ],
+                    ],
+                    [
+                        'range' => [
+                            'id' => [
+                                'gte' => 333,
+                                'lte' => 555,
+                            ],
+                        ],
+                    ],
+                ],
+                'minimum_should_match' => 1,
             ],
         ]);
     }

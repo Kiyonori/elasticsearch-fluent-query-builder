d7023dd5d94445928347578944f8d3b24598b9ee
Kiyonori SAKAI <neco.am@gmail.com>
2025-10-16 05:40:29 +0900

    🔧 # 87 最小のスコアを明記しておく

--- phpinsights.php
+++ phpinsights.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 return [
     'preset' => 'default',
+
+    'requirements' => [
+        'min-quality' => 85,
+        'min-complexity' => 90,
+        'min-architecture' => 85,
+        'min-style' => 90,
+        'disable-security-check' => false,
+    ],
 ];
